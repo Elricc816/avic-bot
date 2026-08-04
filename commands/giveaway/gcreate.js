a97f05a7b64c27f3cca96569cba180b335ae2502
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-04 23:37:04 +0530

    Fix formatting in giveaway command usage message

--- commands/giveaway/gcreate.js
+++ commands/giveaway/gcreate.js
@@ -31,11 +31,11 @@ module.exports = {
                         .setColor("#D3D3D3")
                         .setTitle("<:gift:1514705368759144640> Giveaway Usage")
                         .setDescription(
-"`,gcreate <duration> <winners> <prize>`"
+`**,gcreate <duration> <winners> <prize>**
 
 **Example**
-`,gcreate 1h 1 Discord Nitro`
-                        )
+`,gcreate 1h 1 Discord Nitro``
+)
                 ]
             });
         }
