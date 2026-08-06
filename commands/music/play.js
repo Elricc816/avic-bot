9c6ceb970cf5b9c5e958f86d478a405ef8e627e3
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-07 00:31:51 +0530

    Refactor play command for improved readability

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,30 +1,15 @@
 module.exports = {
-  name: "play",
-
+  name: 'play',
   async execute(message, args, client) {
-
     const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply('<:error:1533894397219831889> Join a voice channel first.');
 
-    if (!voiceChannel) {
-      return message.reply("❌ Join a voice channel first!");
-    }
+    const query = args.join(' ');
+    if (!query) return message.reply('<:error:1533894397219831889> Give me a song name or URL.');
 
-    const query = args.join(" ");
-
-    if (!query) {
-      return message.reply("❌ Provide a song name!");
-    }
-
-    try {
-
-      await client.distube.play(voiceChannel, query, {
-        textChannel: message.channel,
-        member: message.member
-      });
-
-    } catch (err) {
-      console.log("DISTUBE PLAY ERROR:", err);
-      return message.reply("❌ Error playing song 😭 check console");
-    }
-  }
+    await client.distube.play(voiceChannel, query, {
+      textChannel: message.channel,
+      member: message.member,
+    });
+  },
 };
