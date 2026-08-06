d6581582099d8ab90195e10a2cf03c53d3bb012e
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-07 00:34:44 +0530

    Implement Distube music player events

---
+++ events/distube.js
@@ -0,0 +1,29 @@
+const { DisTube } = require('distube');
+const { YouTubePlugin } = require('@distube/youtube');
+
+module.exports = (client) => {
+  client.distube = new DisTube(client, {
+    plugins: [new YouTubePlugin()],
+  });
+
+  client.distube.on('playSong', (queue, song) => {
+    queue.textChannel?.send(`<:play:1533532009450831945> Now playing: **${song.name}** - \`${song.formattedDuration}\``);
+  });
+
+  client.distube.on('addSong', (queue, song) => {
+    queue.textChannel?.send(`<:yes:1533858344341602365> Added **${song.name}** to the queue.`);
+  });
+
+  client.distube.on('error', (channel, error) => {
+    console.error(error);
+    channel?.send('<:error:1533894397219831889> An error occurred while playing music.');
+  });
+
+  client.distube.on('finish', (queue) => {
+    queue.textChannel?.send('<:yes:1533858344341602365> Queue finished.');
+  });
+
+  client.distube.on('empty', (queue) => {
+    queue.textChannel?.send('<:power:1520340458767646821> Voice channel is empty, leaving.');
+  });
+};
