31f0245ad5af6a2b52440c9a2f62e9d01ef3d259
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-07 00:45:21 +0530

    Refactor message sending for song events

--- events/distube.js
+++ events/distube.js
@@ -1,21 +1,29 @@
 const { DisTube } = require('distube');
 const { YouTubePlugin } = require('@distube/youtube');
+const ffmpeg = require('ffmpeg-static');
 
 module.exports = (client) => {
   client.distube = new DisTube(client, {
     plugins: [new YouTubePlugin()],
+    ffmpeg: {
+      path: ffmpeg,
+    },
   });
 
   client.distube.on('playSong', (queue, song) => {
-    queue.textChannel?.send(`<:play:1533532009450831945> Now playing: **${song.name}** - \`${song.formattedDuration}\``);
+    queue.textChannel?.send(
+      `<:play:1533532009450831945> Now playing: **${song.name}** - \`${song.formattedDuration}\``
+    );
   });
 
   client.distube.on('addSong', (queue, song) => {
-    queue.textChannel?.send(`<:yes:1533858344341602365> Added **${song.name}** to the queue.`);
+    queue.textChannel?.send(
+      `<:yes:1533858344341602365> Added **${song.name}** to the queue.`
+    );
   });
 
   client.distube.on('error', (channel, error) => {
-    console.error(error);
+    console.error('DISTUBE ERROR:', error);
     channel?.send('<:error:1533894397219831889> An error occurred while playing music.');
   });
 
@@ -25,5 +33,9 @@ module.exports = (client) => {
 
   client.distube.on('empty', (queue) => {
     queue.textChannel?.send('<:power:1520340458767646821> Voice channel is empty, leaving.');
+  });
+
+  client.distube.on('disconnect', (queue) => {
+    queue.textChannel?.send('<:power:1520340458767646821> Disconnected from the voice channel.');
   });
 };
