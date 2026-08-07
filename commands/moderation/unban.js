47b017ef864f4faf96696d2838e66f366b78099d
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-07 11:51:36 +0530

    Refactor unban command to use error embeds

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,27 +1,34 @@
 const { PermissionsBitField, EmbedBuilder } = require('discord.js');
 
+function errorEmbed(text) {
+  return new EmbedBuilder()
+    .setColor('#D3D3D3')
+    .setDescription(`<:WarningIcon:1514708751385497721> ${text}`);
+}
+
 module.exports = {
   name: 'unban',
   async execute(message, args, client) {
     if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-      return message.reply('<:WarningIcon:1514708751385497721> You don\'t have permission to unban members.');
+      return message.reply({ embeds: [errorEmbed("You don't have permission to unban members.")] });
     }
 
     const userId = args[0];
-    if (!userId) return message.reply('<:WarningIcon:1514708751385497721> Provide the user ID to unban.');
+    if (!userId) {
+      return message.reply({ embeds: [errorEmbed('Provide the user ID to unban.')] });
+    }
 
     const bans = await message.guild.bans.fetch();
     const bannedUser = bans.get(userId);
 
     if (!bannedUser) {
-      return message.reply('<:WarningIcon:1514708751385497721> This user isn\'t banned.');
+      return message.reply({ embeds: [errorEmbed("This user isn't banned.")] });
     }
 
     const reason = args.slice(1).join(' ') || 'No reason provided';
 
     await message.guild.members.unban(userId, reason);
 
-    // Try to DM them — works if the bot already has an open DM channel from a prior ban message
     const dmEmbed = new EmbedBuilder()
       .setColor('#D3D3D3')
       .setTitle('<:WarningIcon:1514708751385497721> You have been unbanned')
