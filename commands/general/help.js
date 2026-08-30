954bea8138a1a0c608e838548c2794af8081bf5a
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-30 13:31:16 +0530

    Enhance help message with command execution details
    Added command execution information to help message.

--- commands/general/help.js
+++ commands/general/help.js
@@ -57,7 +57,7 @@ module.exports = {
       )
       .addFields({
         name: ' ‎',
-        value: `<:slash:1514699274917511248> **Prefix:** \`,\`\n<:dev:1514699929199706143> **Commands:** \`${client.commands.size}\`\n<:general:1514699942181081261> **Modules:** \`5\`\n<:crown:1514699539657920592> **Bot  Owner:** <@${OWNER_ID}>`,
+        value: `<:slash:1514699274917511248> **Prefix:** \`,\`\n<:dev:1514699929199706143> **Commands:** \`${client.commands.size}\`\n<:general:1514699942181081261> **Modules:** \`5\`\n<:crown:1514699539657920592> **Bot Owner:** <@${OWNER_ID}>\n\n-# Command Executed by ${message.author.username}\n-# Thanks for using Fare!`,
       });
 
     const row = new ActionRowBuilder()
