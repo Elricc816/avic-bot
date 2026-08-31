bc3fc28141238ee908e785225ed4dc1b20a985e5
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-08-31 22:31:58 +0530

    Change activity type from 6 to 1

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ client.once('ready', () => {
         activities: [
             {
                 name:`.gg/farehq | Watching ${servers} servers.`,
-                type: 6
+                type: 1
             }
         ]
     });
